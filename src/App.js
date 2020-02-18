02aa9990d1968f82a93ea8fa40487336fbd4d247
adityabisoi <adityabisoi1999@gmail.com>
2020-02-18 13:17:24 +0530

    Inline style with time of day message

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import MainContent from './components/MainContent'
+import MyApp from './components/MyApp'
 
 
 function App()
 {
   return(
     <div>
-      <Header/>
-      <MainContent/>
-      <Footer/>
+      <MyApp/>
     </div>
   )
 }
--- src/components/MyApp.js
+++ src/components/MyApp.js
@@ -1,7 +1,29 @@
 import React from 'react'
+
 function MyApp(){
+    const date = new Date()
+    const hours = date.getHours()
+    let message
+
+    const styles = {
+        fontSize:20
+    }
+
+    if(hours<12){
+        message = "Good Morning" 
+        styles.color = "red"
+    }
+    else if(hours>12 && hours< 18){
+        message = "Good Afternoon"
+        styles.color = "blue"
+    }
+    else{
+        message = "Good Evening"
+        styles.color = "green"
+    }
+    
     return (
-        <h1>Hello World</h1>
+    <h1 style={styles}>{message}</h1>
     )
 }
 
