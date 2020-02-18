80e01aa8f493cc42d0bf9d8ca94594106b517959
adityabisoi <adityabisoi1999@gmail.com>
2020-02-18 20:56:05 +0530

    ContactCard props added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import MyApp from './components/MyApp'
+import React from "react"
+import ContactCard from './components/ContactCard'
 
+function App() {
+    return (
+        <div className="contacts">
 
-function App()
-{
-  return(
-    <div>
-      <MyApp/>
-    </div>
-  )
+            <ContactCard 
+                name="Mr. Whiskerson" 
+                imgUrl="http://placekitten.com/300/200"
+                phone="(212) 555-1234" 
+                email="mr.whiskaz@catnap.meow"
+            />
+            
+            <ContactCard 
+                name="Fluffykins" 
+                imgUrl="http://placekitten.com/400/200" 
+                phone="(212) 555-2345" 
+                email="fluff@me.com"
+            />
+            
+            <ContactCard 
+                name="Destroyer" 
+                imgUrl="http://placekitten.com/400/300" 
+                phone="(212) 555-3456" 
+                email="ofworlds@yahoo.com"
+            />
+            
+            <ContactCard 
+                name="Felix" 
+                imgUrl="http://placekitten.com/200/100" 
+                phone="(212) 555-4567" 
+                email="thecat@hotmail.com"
+            />
+        </div>
+    )
 }
 
-export default App;
+export default App
