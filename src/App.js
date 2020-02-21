411fd627e047cbef446d2384ed00d28f55c50a19
adityabisoi <adityabisoi1999@gmail.com>
2020-02-21 12:26:39 +0530

    joke component with question missing

--- src/App.js
+++ src/App.js
@@ -1,45 +1,37 @@
 import React from "react"
-import ContactCard from './components/ContactCard'
+import Joke from './components/Joke'
 
 function App() {
     return (
-        <div className="contacts">
+        <div>
 
-            <ContactCard 
-            contact={{
-                name:"Mr. Whiskerson",
-                imgUrl:"http://placekitten.com/300/200",
-                phone:"(212) 555-1234",
-                email:"mr.whiskaz@catnap.meow"
-            }}
-                
+            <Joke 
+                punchLine="It’s hard to explain puns to kleptomaniacs because they always take things literally." 
             />
             
-            <ContactCard 
-            contact={{
-                name:"Fluffykins" ,
-                imgUrl:"http://placekitten.com/400/200" ,
-                phone:"(212) 555-2345" ,
-                email:"fluff@me.com"
-              }}
+            <Joke 
+                question="What's the best thing about Switzerland?" 
+                punchLine="I don't know, but the flag is a big plus!"
             />
             
-            <ContactCard 
-            contact={{
-                name:"Destroyer" ,
-                imgUrl:"http://placekitten.com/400/300" ,
-                phone:"(212) 555-3456" ,
-                email:"ofworlds@yahoo.com"
-              }}
+            <Joke 
+                question="Did you hear about the mathematician who's afraid of negative numbers?" 
+                punchLine="He'll stop at nothing to avoid them!"
             />
             
-            <ContactCard 
-            contact={{
-                name:"Felix" ,
-                imgUrl:"http://placekitten.com/200/100" ,
-                phone:"(212) 555-4567" ,
-                email:"thecat@hotmail.com"
-              }}
+            <Joke 
+                question="Hear about the new restaurant called Karma?" 
+                punchLine="There’s no menu: You get what you deserve."
+            />
+            
+            <Joke 
+                question="Did you hear about the actor who fell through the floorboards?" 
+                punchLine="He was just going through a stage."
+            />
+            
+            <Joke 
+                question="Did you hear about the claustrophobic astronaut?" 
+                punchLine="He just needed a little space."
             />
         </div>
     )
